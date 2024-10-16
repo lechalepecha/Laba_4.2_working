3a629992bb83d0dc20f58d6c49e946611fd3fe47
lechalepecha <aleksejserakov45@gmail.com>
2024-10-16 19:54:12 +0700

    log

--- app/src/main/java/com/example/laba4/MainActivity.kt
+++ app/src/main/java/com/example/laba4/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import android.widget.TextView
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+private const val TAG="MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -50,11 +54,38 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener()
         {
             currentIndex=(currentIndex+1)%questionBank.size
-            updateQuestin()
+            updateQuestion()
         }
-        updateQuestin()
+        updateQuestion()
     }
-    private fun updateQuestin(){
+
+    override fun onStart()
+    {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+    override fun onResume()
+    {
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
+    override fun onPause()
+    {
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+    }
+    override fun onStop()
+    {
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+    override fun onDestroy()
+    {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
+
+    private fun updateQuestion(){
         val questionTextResId=questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
@@ -69,6 +100,7 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show()
+        //updateQuestion()
     }
 
 }
