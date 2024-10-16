0137b5e8ac6bf3b81d750a0dfae7847245161e78
lechalepecha <aleksejserakov45@gmail.com>
2024-10-16 20:33:58 +0700

    CHEATS!!!

---
+++ app/src/main/java/com/example/laba4/CheatActivity.kt
@@ -0,0 +1,18 @@
+package com.example.laba4
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+class CheatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.cheats)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
