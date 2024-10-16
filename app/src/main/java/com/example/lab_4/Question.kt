1ac66ceeff5f78eaa39421a7b2bc699f54728900
lechalepecha <aleksejserakov45@gmail.com>
2024-10-16 19:36:39 +0700

    new functions

--- app/src/main/java/com/example/lab_4/Question.kt
+++ app/src/main/java/com/example/lab_4/Question.kt
@@ -1,4 +1,4 @@
-package com.example.lab_4
+package com.example.laba4
 import androidx.annotation.StringRes
 data class Question(@StringRes val textResId:
                     Int, val answer: Boolean)
